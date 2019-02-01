31ce0cef8f5f70609d74fd2f45c0bd13501fa791
abinash1712 <abinash.nayak1712@gmail.com>
2019-02-01 18:14:54 +0530

    changing local mongodb to cloud db(mLab)

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/devconnector",
+  mongoURI:
+    "mongodb://abinash1712:abinash1712@ds111065.mlab.com:11065/devconnector",
+  //mongoURI: "mongodb://localhost:27017/devconnector",
   secretOrKey: "jwtSecretKey"
 };
